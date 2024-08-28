b6961b20bad4ee2df6c3c752177e65b0fa13d2b7
Tajammul.Mustafa <tajammul.mustafa@e-mundo.de>
2024-08-28 02:49:56 +0200

    feat: setup car client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarComponent } from './car/car.component';
+import { Car } from './car/car.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'front-end';
